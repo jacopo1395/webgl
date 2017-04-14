409c85d90faeacf2d7957bad039ac6680c05356c
jacopo <jack1395@hotmail.it>
2017-04-14 12:34:37 +0200

    add original

--- Homework1/Homework1/Homework1.js
+++ Homework1/Homework1/Homework1.js
@@ -17,6 +17,43 @@ var c;
 
 var flag = true;
 
+/*
+███    ███ ██    ██
+████  ████  ██  ██
+██ ████ ██   ████
+██  ██  ██    ██
+██      ██    ██
+*/
+
+var normalsArray = [];
+
+var ambientColor, diffuseColor, specularColor;
+
+var modelView, projection;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var normalMatrix, normalMatrixLoc;
+
+
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialShininess = 100.0;
+/*
+
+█████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████
+*/
+
+
+
+
 var image1 = new Uint8Array(4 * texSize * texSize);
 
 for (var i = 0; i < texSize; i++) {
@@ -108,29 +145,58 @@ function configureTexture() {
 }
 
 function quad(a, b, c, d) {
+    /*
+    ███    ███ ██    ██
+    ████  ████  ██  ██
+    ██ ████ ██   ████
+    ██  ██  ██    ██
+    ██      ██    ██
+    */
+
+
+    var t1 = subtract(vertices[b], vertices[a]);
+    var t2 = subtract(vertices[c], vertices[b]);
+    var normal = cross(t1, t2);
+    var normal = vec3(normal);
+
+
+
     pointsArray.push(vertices[a]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[0]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[b]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[1]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[c]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[2]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[a]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[0]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[c]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[2]);
+    normalsArray.push(normal);
 
     pointsArray.push(vertices[d]);
     colorsArray.push(vertexColors[a]);
     texCoordsArray.push(texCoord[3]);
+    normalsArray.push(normal);
+
+    /*
+
+    █████ █████ █████ █████ █████
+    */
+
+
 }
 
 function colorCube() {
@@ -165,6 +231,14 @@ window.onload = function init() {
 
     colorCube();
 
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+
+    var vNormal = gl.getAttribLocation( program, "vNormal" );
+    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vNormal );
+
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
@@ -189,6 +263,35 @@ window.onload = function init() {
     gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vTexCoord);
 
+
+    projection = ortho(-1, 1, -1, 1, -100, 100);
+
+    /*
+    ███    ███ ██    ██
+    ████  ████  ██  ██
+    ██ ████ ██   ████
+    ██  ██  ██    ██
+    ██      ██    ██
+    */
+
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+
+    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
+    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projection));
+
+    /*
+
+    █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████ █████
+    */
 
 
 
@@ -225,6 +328,13 @@ var render = function() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     if (flag) theta[axis] += 2.0;
     gl.uniform3fv(thetaLoc, theta);
+    modelView = mat4();
+    modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0] ));
+    modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
+    modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1] ));
+
+    gl.uniformMatrix4fv( gl.getUniformLocation(program,
+            "modelViewMatrix"), false, flatten(modelView) );
     gl.drawArrays(gl.TRIANGLES, 0, numVertices);
     requestAnimFrame(render);
 }
